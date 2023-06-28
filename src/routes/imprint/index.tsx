27b93bdea681b86f3731295eed72c189c8973f76
Johannes Rackles <johannes.rackles@brandung.de>
2023-06-28 11:23:04 +0200

    add impressum

---
+++ src/routes/imprint/index.tsx
@@ -0,0 +1,34 @@
+import { component$ } from "@builder.io/qwik";
+import type { DocumentHead } from "@builder.io/qwik-city";
+
+export default component$(() => {
+  return (
+    <div class="prose mx-auto">
+      <h1>Impressum</h1>
+      <h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+      <p>
+        Johannes Rackles
+        <br />
+        Colbestr 25
+        <br />
+        10247 Berlin
+      </p>
+      <h2>Kontakt</h2>
+      <p>
+        Telefon: &#91;Telefonnummer&#93;
+        <br />
+        E-Mail: contact@rackles.io
+      </p>
+    </div>
+  );
+});
+
+export const head: DocumentHead = {
+  title: "rackles.io | Imprint",
+  meta: [
+    {
+      name: "description",
+      content: "Contact John Rackles",
+    },
+  ],
+};
